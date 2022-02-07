c1c8e02e2d74a372024b0ce519450ca0c94edcc9
Lenard-0 <lenard@insanemarketing.com.au>
2022-02-07 17:29:47 +1030

    remove bs comments

--- parser-and-schema/src/parser/schema_parser/enums/parse_enum.rs
+++ parser-and-schema/src/parser/schema_parser/enums/parse_enum.rs
@@ -4,32 +4,6 @@ use shared::CastleError;
 
 use crate::{tokenizer::tokenizer::{self, Tokenizer}, ast::syntax_definitions::{enum_definition::{EnumDefinition, EnumDataType, EnumVariant}, keyword::Keyword}, parser::schema_parser::{parse_schema_type::{get_identifier_skip_open_block,}, parse_schema_field::get_identifier}, token::token::{TokenKind, Punctuator, Identifier}};
 
-/// enum Color {
-///  Red,
-/// Green,
-///  Blue
-/// }
-
-/// takes in tokenizer and returns parsed type
-///    - start loop
-///    - if next token is identifier, parse identifier
-///    - call next token to skip openblock
-///    - if next token is identifier, parse enum-variant
-///    - else if next token is closeblock, break loop
-///    - return parsed type
-
-// fn parse_schema_type<R>(tokenizer: &mut Tokenizer<R>) -> Result<SchemaType, CastleError> 
-// where R: Read{
-//     let mut fields = HashMap::new();
-//     let token = tokenizer.next(true)?;
-//     let identifier = get_identifier_skip_open_block(token, tokenizer)?;
-
-//     loop {
-//         let end_of_schema_type = check_token_and_parse_schema_field_or_break(tokenizer, &mut fields)?;
-//         if end_of_schema_type { break; }
-//     }
-//     return Ok(SchemaType { identifier, fields });
-
 pub fn parse_enum_definition<R>(tokenizer: &mut tokenizer::Tokenizer<R>) -> Result<EnumDefinition, CastleError>
     where R: Read {
         let mut variants = HashMap::new();
