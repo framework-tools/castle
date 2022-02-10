46ea69752c6469a1b6db80d92ee501145a25d14c
Lenard-0 <lenard@insanemarketing.com.au>
2022-02-11 08:17:26 +1030

    commit

---
+++ castle/src/directives/directives.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+use parser_and_schema::ast::syntax_definitions::fn_definition::FnDefinition;
+use shared::CastleError;
+
+pub struct Directive {
+    pub resolver_definition: DirectiveDefinition,
+    // unsure about this field currently
+    // pub function: fn(args: &HashMap<String, String>) -> Result<String, CastleError>,
+}
+
+pub fn generate_directives() -> Result<HashMap<Box<str>, DirectiveDefinition>, CastleError>{
+    let mut directives = HashMap::new();
+
+    
+    return Ok(directives)
+}
