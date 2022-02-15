1d55bd9058c505746d8424ec70537dc4586cbd7d
Lenard-0 <lenard@insanemarketing.com.au>
2022-02-16 10:26:13 +1030

    adding tests

--- castle/tests/tests_validate_resolvers_to_schema.rs
+++ castle/tests/tests_validate_resolvers_to_schema.rs
@@ -4,6 +4,14 @@ use castle::{validation::validate_schema_with_functions::validate_schema_with_re
 use parser_and_schema::{parsers::schema_parser::{parse_schema::parse_schema, types::{type_system::Type, primitive_type::PrimitiveType}}, ast::syntax_definitions::{fn_definition::FnDefinition, argument::{ArgumentOrTuple, IdentifierAndTypeArgument, IdentifierAndValueArgument}, directive_definition::{DirectiveDefinition, DirectiveOnValue, }}};
 use shared::CastleError;
 
+/// Currently Testing:
+/// Breaks if resolver defined in schema is not in the resolver map
+/// Breaks if resolver defined in schema has same arguments but different types
+/// Breaks if resolver defined in schema has different arguments (both ways)
+/// Breaks if resolver defined in schema has different return type
+/// Breaks if directive defined in schema does not exist in directive map
+/// Breaks if directive in schema has different arguments (types, and different lengths)
+
 #[cfg(test)]
 #[test]
 fn test_resolver_defined_in_schema_that_does_not_exist_throws_error(){
