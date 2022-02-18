efa2a9ee1bf67d66122028a401068ceb9fb16886
Will <will@framework.tools>
2022-02-18 11:57:36 +1030

    fixed error

--- castle/src/castle_object/castle_struct.rs
+++ castle/src/castle_object/castle_struct.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap};
 use parser_and_schema::{ast::syntax_definitions::{schema_definition::{SchemaDefinition}, fn_definition::FnDefinition, directive_definition::{self, DirectiveDefinition}}, parsers::{schema_parser::parse_schema::parse_schema, query_parser::parse_query::parse_query}};
 use shared::CastleError;
 
-use crate::{resolvers::{resolver_map::ResolverMap, resolve_query_wants::resolve_all_wants, resolver_type::{TopLevelResolvers, Resolver}, individual_resolvers::page_resolvers::basic_page_info::basic_page_info}, directives::directives::DirectiveMap, validation::{self_validation_schema::self_validate_schema::self_validate_schema, validate_backend_fns_with_schema::validate_backend_fns_with_schema::validate_schema_with_resolvers_and_directives, validate_query_with_schema::validate_query_with_schema::validate_query_with_schema}, castle_schema::castle_schema::SCHEMA};
+use crate::{resolvers::{resolver_map::{ResolverMap, self, create_resolver_map_with_all_resolvers}, resolve_query_wants::resolve_all_wants, resolver_type::{TopLevelResolvers, Resolver}, individual_resolvers::page_resolvers::basic_page_info::basic_page_info}, directives::directives::DirectiveMap, validation::{self_validation_schema::self_validate_schema::self_validate_schema, validate_backend_fns_with_schema::validate_backend_fns_with_schema::validate_schema_with_resolvers_and_directives, validate_query_with_schema::validate_query_with_schema::validate_query_with_schema}, castle_schema::castle_schema::SCHEMA};
 
 pub struct Castle<C, R>{
     pub resolver_map: ResolverMap<C, R>,
@@ -14,13 +14,13 @@ pub struct Castle<C, R>{
 impl<C, R> Castle<C, R> {
 
     pub fn build_and_validate(
-        resolvers: ResolverMap<C, R>,
+        resolver_map: ResolverMap<C, R>,
         directives: DirectiveMap<C, R>,
         schema: &str,
     ) -> Result<Castle<C, R>, CastleError> {
         let parsed_schema = parse_schema(&schema)?;
         let castle = Castle {
-            resolvers,
+            resolver_map,
             parsed_schema,
             directives
         };
@@ -35,7 +35,7 @@ impl<C, R> Castle<C, R> {
     /// - Validate schema resolvers & directives (functions) match the ones we've built in Rust
     pub fn validate(&self) -> Result<(), CastleError> {
         self_validate_schema(&self.parsed_schema)?;
-        validate_schema_with_resolvers_and_directives(&self.parsed_schema, &self.resolvers, &self.directives)?;
+        validate_schema_with_resolvers_and_directives(&self.parsed_schema, &self.resolver_map, &self.directives)?;
         return Ok(())
     }
 
@@ -45,7 +45,7 @@ impl<C, R> Castle<C, R> {
     pub fn parse_query_resolve_wants(&self, query: &str, context: C) -> Result<TopLevelResolvers<R>, CastleError> {
         let parsed_query = parse_query(query)?;
         validate_query_with_schema(&parsed_query, &self.parsed_schema)?;
-        let resolved_wants = resolve_all_wants(parsed_query.wants, &self.resolvers, context)?;
+        let resolved_wants = resolve_all_wants(parsed_query.wants, &self.resolver_map, context)?;
         return Ok(resolved_wants)
     }
 }
@@ -59,7 +59,7 @@ pub struct CastleBuilder<'a, C, R> {
 impl<'a, C, R> CastleBuilder<'a, C, R> {
     pub fn new() -> Self {
         Self {
-            resolvers: ResolverMap::new(),
+            resolver_map: ResolverMap::new(),
             schema: None,
             directives: HashMap::new(),
         }
@@ -82,11 +82,11 @@ impl<'a, C, R> CastleBuilder<'a, C, R> {
         else {
             schema = self.schema.unwrap();
         }
-        Castle::build_and_validate(self.resolvers, self.directives, schema)
+        Castle::build_and_validate(self.resolver_map, self.directives, schema)
     }
 
     pub fn add_resolver(&mut self, resolver_name: &str, resolver: Resolver<C, R>) {
-        self.resolvers.resolvers.insert(resolver_name.into(), resolver);
+        self.resolver_map.resolvers.insert(resolver_name.into(), resolver);
     }
 
     pub fn add_directive(&mut self, directive_name: &str, directive: Resolver<C, R>) {
@@ -94,6 +94,6 @@ impl<'a, C, R> CastleBuilder<'a, C, R> {
     }
 
     pub fn add_all_resolvers(&mut self) {
-        self.resolvers.resolvers = create_resolver_map_with_all_resolvers();
+        self.resolver_map.resolvers = create_resolver_map_with_all_resolvers();
     }
 }
