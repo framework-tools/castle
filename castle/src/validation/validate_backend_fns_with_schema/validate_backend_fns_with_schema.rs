10e5dd62d1ace07ee992a2beda4c2227534bb5c0
Lenard-0 <lenard@insanemarketing.com.au>
2022-04-12 09:41:17 +0930

    cleaned resolver & directives to schema validation

--- castle/src/validation/validate_backend_fns_with_schema/validate_backend_fns_with_schema.rs
+++ castle/src/validation/validate_backend_fns_with_schema/validate_backend_fns_with_schema.rs
@@ -1,94 +1,49 @@
-use std::collections::{HashSet};
-
+use std::{collections::HashMap};
 use parser_and_schema::ast::syntax_definitions::{schema_definition::SchemaDefinition};
 use shared::castle_error::CastleError;
+use crate::{resolvers::{resolver_map::ResolverMap}, directives::{directives::DirectiveMap}};
 
-
-use crate::{resolvers::{resolver_map::ResolverMap}, directives::{directives::DirectiveMap, self}};
-
-
-
-/// Cross-Validation
-///  For Functions:
-///  - Resolvers
-///  - Directives
-/// 
-/// Every resolver correlates to a function (inside Castle)
-/// 
-/// pub struct Resolver {
-///      pub resolver_definition: FnDefinition,
-///      unsure about field below currently
-///      pub function: fn(args: &HashMap<String, String>) -> Result<String, CastleError>,
-///  }
-/// 
-/// Resolvers is: HashMap<Box<str>, Resolver>
 /// Need to check:
 /// - Every function in schema has a resolver
 /// - Every fields' directives have a resolver
-/// 
-/// 
-/// 
-
-pub fn validate_schema_with_resolvers_and_directives<C, T>(
-    parsed_schema: &SchemaDefinition,
-    resolvers: &ResolverMap<C, T>,
-    directives: &DirectiveMap<C, T>,
+pub(crate) fn validate_schema_with_resolvers_and_directives<C, R>(
+    schema: &SchemaDefinition,
+    resolvers: &ResolverMap<C, R>,
+    directives: &DirectiveMap<C, R>,
 ) -> Result<(), CastleError> {
-    let resolvers_identifiers = get_resolvers_identifiers(&resolvers);
-    let directives_identifiers = get_directives_identifiers(&directives);
-    validate_schema_with_resolvers(resolvers, parsed_schema)?;
-    validate_schema_with_directives(&directives, parsed_schema)?;
+    validate_functions_are_defined_in_schema(
+        &schema.functions, 
+        &resolvers.resolvers,
+        true
+    )?; 
+    validate_functions_are_defined_in_schema(
+        &schema.directives, 
+        directives,
+        true
+    )?; 
     return Ok(())
 }
 
-fn get_resolvers_identifiers<C, T>(resolver_map: &ResolverMap<C, T>) -> HashSet<&Box<str>>{
-    let mut resolvers_identifiers = HashSet::new();
-    for (resolver_identifier, _) in &resolver_map.resolvers {
-        resolvers_identifiers.insert(resolver_identifier);
-    }
-    return resolvers_identifiers
+pub fn validate_functions_are_defined_in_schema<T, U>(
+    definitions: &HashMap<Box<str>, T>, 
+    fns: &HashMap<Box<str>, U>, 
+    for_resolver: bool
+) -> Result<Vec<()>, CastleError> {
+    let result: Result<Vec<()>, CastleError> = fns.into_iter()
+        .map(|(key, _)| validate_fn_is_defined_in_schema(key, definitions, for_resolver))
+        .collect();
+    return result
 }
 
-fn get_directives_identifiers<C, T>(directives: &DirectiveMap<C, T>) -> HashSet<&Box<str>>{
-    let mut directives_identifiers = HashSet::new();
-    for (directive_identifier, _) in directives {
-        directives_identifiers.insert(directive_identifier);
+pub(crate) fn validate_fn_is_defined_in_schema<T>(
+    key: &Box<str>,
+    definitions: &HashMap<Box<str>, T>, 
+    for_resolver: bool
+) -> Result<(), CastleError> {
+    return match definitions.contains_key(key) {
+        true => Ok(()),
+        false if for_resolver => Err(CastleError::UndefinedResolver(format!("Resolver not found for fn definition in schema: {}", key).into())), 
+        _ => Err(CastleError::UndefinedDirective(format!("Directive not found for directive definition in schema: {}", key).into())), 
     }
-    return directives_identifiers
 }
 
-/// Steps For valiate_schema_with_resolvers():
-/// - Generate resolvers
-/// - For each fn in schema.functions
-///     - Check if fn has a resolver by using the identifier of the fn
-///     - If hashmap returns None for the identifier, throw error
-///     - Else, unwrap the resolver and continue
-///     - For the resolver, check the fn definition in schema & fn definition in resolvers is identical
-///     - Else throw error
-pub fn validate_schema_with_resolvers<C, R>(resolver_map: &ResolverMap<C, R>, parsed_schema: &SchemaDefinition ) -> Result<(), CastleError> {
-    for resolver_in_schema in parsed_schema.functions.values() {
-        let resolver = resolver_map.resolvers.get(&resolver_in_schema.name);
-        if resolver.is_none() {
-            return Err(CastleError::UndefinedResolver(format!("Resolver not found for fn definition in schema: {}", resolver_in_schema.name).into()))
-        }
-    }
-    return Ok(())
-}
-
-/// Steps For validate_schema_with_directives():    
-/// - For each field in schema.fields
-///    - Check if field has a directive by using the identifier of the field
-///   - If hashmap returns None for the identifier, throw error
-///   - Else, unwrap the directive and continue
-///     - For the directives, check the fn definition in schema & directive definition in resolvers is identical
-///     - Else throw error
-///   - If no errors, return Ok(())
-pub fn validate_schema_with_directives<C, R>(directives: &DirectiveMap<C, R>, parsed_schema: &SchemaDefinition) -> Result<(), CastleError> {
-    for (identifier, directive_in_schema) in &parsed_schema.directives {
-        let directive = directives.get(identifier);
-        if directive.is_none() {
-            return Err(CastleError::UndefinedDirective(format!("Directive not found for directive definition in schema: {}", directive_in_schema.function.name).into()))
-        }
-    }
-    return Ok(())
-}
--- castle/tests/tests_validate_backend_to_schema.rs
+++ castle/tests/tests_validate_backend_to_schema.rs
@@ -1,10 +1,11 @@
+use castle::validation::validate_backend_fns_with_schema::validate_backend_fns_with_schema::validate_functions_are_defined_in_schema;
 use parser_and_schema::parsers::schema_parser::parse_schema::parse_schema;
 use std::collections::HashMap;
 use parser_and_schema::ast::syntax_definitions::want::Want;
-use castle::{resolvers::resolver_type::Args, validation::validate_backend_fns_with_schema::validate_backend_fns_with_schema::validate_schema_with_directives};
+use castle::{resolvers::resolver_type::Args};
 use castle::castle_object::resolver_return_types::Value;
 use shared::castle_error::CastleError;
-use castle::{castle_object::castle_struct::CastleBuilder, validation::validate_backend_fns_with_schema::validate_backend_fns_with_schema::validate_schema_with_resolvers};
+use castle::{castle_object::castle_struct::CastleBuilder};
 
 /// Currently Testing:
 /// - Breaks if resolver defined in schema is not in the resolver map
@@ -13,6 +14,8 @@ use castle::{castle_object::castle_struct::CastleBuilder, validation::validate_b
 #[cfg(test)]
 #[test]
 fn test_resolver_defined_in_schema_that_does_not_exist_throws_error(){
+    use castle::validation::validate_backend_fns_with_schema::validate_backend_fns_with_schema::validate_functions_are_defined_in_schema;
+
     let schema = "
     fn foo(id: Int) -> Int
     fn me (name: String) -> String 
@@ -24,7 +27,11 @@ fn test_resolver_defined_in_schema_that_does_not_exist_throws_error(){
     }
     let mut builder: CastleBuilder<(), ()> = CastleBuilder::new();
     builder.add_resolver("random_resolver".into(), random_resolver);
-    let result = validate_schema_with_resolvers(&builder.resolver_map, &parsed_schema);
+    let result = validate_functions_are_defined_in_schema(
+        &parsed_schema.functions,
+        &builder.resolver_map.resolvers, 
+        true
+    );
     if result.is_err() {
         match result {
             Err(CastleError::UndefinedResolver(_)) => {}, //passes
@@ -50,7 +57,11 @@ fn test_directive_defined_in_schema_that_does_not_exist_throw_error(){
     let mut builder: CastleBuilder<(), ()> = CastleBuilder::new();
     builder.add_directive("random_directive".into(), random_directive);
 
-    let result = validate_schema_with_directives(&builder.directives, &parsed_schema);
+    let result = validate_functions_are_defined_in_schema(
+        &parsed_schema.directives,
+        &builder.directives, 
+        false
+    );
     
     if result.is_err() {
         match result {
