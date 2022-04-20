0cb5bc3ff13744e5d784c6d862566592b2655c0e
Albert <albert@framework.tools>
2022-04-20 15:50:11 +0930

    add projection type

--- query_parser/src/types/mod.rs
+++ query_parser/src/types/mod.rs
@@ -1,10 +1,2 @@
 
-mod query;
 mod projection;
-
-/// unknown
-/// match
-
-// in projection, can rename fields using as keyword
-// /// first_name as name
-// pub as_name: Option<Box<str>>,
---
+++ query_parser/src/types/projection.rs
@@ -0,0 +1,49 @@
+
+
+/// The query
+///
+/// ```gql
+/// me {
+///     first_name
+///     profile_pic(
+///         size: 100
+///     ) as large_pic
+///     profile_pic(size: 50) as small_pic
+///     friends [
+///         first_name
+///     ]
+/// }
+/// ```
+///
+/// The returned json
+///
+/// ```json
+/// {
+///     "me": {
+///         "first_name": "Albert",
+///         "large_pic": "https://...",
+///         "small_pic": "https://...",
+///         "friends": [
+///             {
+///                 "first_name": "Gerard"
+///             },
+///             {
+///                 "first_name": "Will"
+///             }
+///         ]
+///     }
+/// }
+/// ```
+struct Projection {
+    pub name: Box<str>,
+    pub inputs: Vec<Input>,
+    pub rename: Option<Box<str>>, // original as renamed
+    pub kind: ProjectionKind,
+}
+
+
+enum ProjectionKind {
+    Object(HashMap<Box<str>, Projection>),
+    List(HashMap<Box<str>, Projection>),
+    Field
+}
