789ab7f375da6832e7c02c95bc546370a2e3af08
Albert <albert@framework.tools>
2022-05-02 13:20:05 +0930

    refactor and improve code, add some tests

--- castle/src/validation/validate_inputs/mod.rs
+++ castle/src/validation/validate_inputs/mod.rs
@@ -1,15 +1,11 @@
 use castle_error::CastleError;
 use query_parser::{Input, Inputs};
-use schema_parser::types::{SchemaDefinition, InputDefinition, Kind, InputDefinitions};
+use schema_parser::types::{
+    DirectiveLocation, InputDefinition, InputDefinitions, Kind, SchemaDefinition,
+};
 use shared_parser::Primitive;
 
-use crate::{Projection};
-
-use super::join_paths;
-
-pub(crate) fn validate_inputs(schema: &SchemaDefinition, query: Projection) -> Result<(), CastleError> {
-    unimplemented!()
-}
+use super::{join_paths, validate_schema::validate_directives::validate_directive};
 
 /// we have an [InputDefinition], which has a [input_kind](Kind).
 /// we want to validate that the type the user provided as [Input]
@@ -20,83 +16,56 @@ pub(crate) fn type_check_input_against_input_definition(
     input_def: &InputDefinition,
     input_value: &Input,
 ) -> Result<(), CastleError> {
-    match &input_def.input_kind {
-        Kind { ident, .. } if &**ident == "String" =>  match input_value {
-            Input::Primitive(Primitive::String(..)) => Ok(()),
-            _ => Err(CastleError::Validation(format!(
-                "{} expected input of type String but got {:#?}",
-                join_paths(path),
-                input_value
-            ).into()))
-        }
-        Kind { ident, .. } if &**ident == "number" =>  match input_value {
-            Input::Primitive(Primitive::Number(..)) => Ok(()),
-            _ => Err(CastleError::Validation(format!(
-                "{} expected input of type Int but got {:#?}",
-                join_paths(path),
-                input_value
-            ).into()))
-        }
-        Kind { ident, .. } if &**ident == "bool" =>  match input_value {
-            Input::Primitive(Primitive::Boolean(..)) => Ok(()),
-            _ => Err(CastleError::Validation(format!(
-                "{} expected input of type Boolean but got {:#?}",
-                join_paths(path),
-                input_value
-            ).into()))
-        }
-        Kind { ident, generics } if &**ident == "Option" =>  match input_value {
-            Input::Primitive(Primitive::String(..)) => Ok(()),
-            _ => Err(CastleError::Validation(format!(
-                "{} expected input of type Option but got {:#?}",
-                join_paths(path),
-                input_value
-            ).into()))
-        }
-        Kind { ident, generics } if &**ident == "Vec" =>  match input_value {
-            Input::Primitive(Primitive::String(..)) => Ok(()),
-            _ => Err(CastleError::Validation(format!(
-                "{} expected input of type Option but got {:#?}",
-                join_paths(path),
-                input_value
-            ).into()))
-        }
-        // TODO: custom types
+    // TODO: maybe mutate the input to include the default value?
+
+    // we will first check the input kind matches the expected type
+    type_check_input_against_expected_type(schema, path, &input_def.input_kind, input_value)?;
+
+    // typecheck each of the input directives
+    for input_directive in input_def.directives.iter() {
+        validate_directive(
+            schema,
+            path,
+            input_directive,
+            DirectiveLocation::InputFieldDefinition,
+        )?;
+    }
+
+    Ok(())
+}
+pub(crate) fn type_check_input_against_expected_type(
+    schema: &SchemaDefinition,
+    path: &[&str], // used to build error message
+    expected_kind: &Kind,
+    input_value: &Input,
+) -> Result<(), CastleError> {
+    match input_value {
+        // TODO: maybe implement scalar types?
         // TODO: Uuid
         // TODO: Option
-        // TODO: Vec
         // TODO: Enum
-        //# eg:
-        //# ```text
-        //# type CreateUser {
-        //#     name: String
-        //#     age: number
-        //# }
-        //# type Query {
-        //#     createUser(input: CreateUser): User
-        //# }
-        //# ```
-        Kind { ident, .. } => match schema.input_types.get(ident) {
-            Some(input_def) => match input_value {
-                Input::Map(map) => {
-                    type_check_inputs_against_input_definitions(
-                        schema,
-                        path,
-                        &input_def.input_definitions,
-                        map,
-                    )
-                },
-                other => Err(CastleError::Validation(format!(
-                    "{} expected input of type {:#?} but got {:#?}",
-                    join_paths(path),
-                    &input_def.input_definitions,
-                    other
-                ).into()))
-            },
-            // This case should not happen if we properly validate the schema.
-            None => Err(CastleError::Validation(format!("InputTypeDefinition for kind {} not found in schema", input_def.input_kind).into()))
-        }
+        Input::Primitive(Primitive::String(..)) if &*expected_kind.ident == "String" => {}
+        Input::Primitive(Primitive::Number(..)) if &*expected_kind.ident == "number" => {}
+        Input::Primitive(Primitive::Boolean(..)) if &*expected_kind.ident == "bool" => {}
+        Input::List(list) if &*expected_kind.ident == "bool" => for (index, input) in list.iter().enumerate() {
+            type_check_input_against_expected_type(schema, &[path, &[&format!("{}", index)]].concat(), &expected_kind.generics[0], input)?;
+        },
+        Input::Map(map) if let Some(input_def) = schema.input_types.get(&expected_kind.ident) =>
+            type_check_inputs_against_input_definitions(
+                schema,
+                path,
+                &input_def.input_definitions,
+                map,
+            )?,
+        input_value => Err(CastleError::Validation(format!(
+            "{} expected input of type {:#?} but got {}",
+            join_paths(path),
+            expected_kind,
+            input_value
+        ).into()))?,
     }
+
+    Ok(())
 }
 
 /// We want to validate that the user provided all of the required inputs and no
@@ -111,41 +80,61 @@ pub(crate) fn type_check_inputs_against_input_definitions(
     input_defs: &InputDefinitions,
     map: &Inputs,
 ) -> Result<(), CastleError> {
-    for def in input_defs.values() {
-        match map.get(&def.ident) {
-            Some(input_value) => {
-                type_check_input_against_input_definition(
-                    schema,
-                    &[path, &[&*def.ident]].concat(),
-                    def,
-                    input_value,
-                )?;
-            }
-            None => Err(CastleError::Validation(format!("{} expected input of type {:#?} but got nothing",
-                join_paths(path),
-                def
-            ).into()))?
-        }
-    }
-    // check for extra inputs
-    for (ident, ..) in map.iter() {
-        // check that the input definition actually has an input definition for this user-provided input
-        match input_defs.get(&*ident) {
-            Some(_) => (),
-            None => Err(CastleError::Validation(format!("{} found unrecognised input {:#?}",
-                join_paths(&[path, &[&*ident]].concat()),
-                ident
-            ).into()))?
-        }
-    }
+    check_for_unspecified_args(schema, path, input_defs, map)?;
+    check_for_missing_args(path, input_defs, map)?;
     Ok(())
 }
 
-pub(crate) fn type_check_generic(
+/// check that there are no unspecified args being used in the inputs
+/// that were not defined in the [InputDefinitions]
+/// in other words, check that each [Input] was defined in the [InputDefinition]
+pub(crate) fn check_for_unspecified_args(
     schema: &SchemaDefinition,
     path: &[&str],
     input_defs: &InputDefinitions,
-    map: &Inputs,
+    inputs_map: &Inputs,
 ) -> Result<(), CastleError> {
-    unimplemented!()
+    for (arg_ident, input_value) in inputs_map.iter() {
+        match input_defs.get(arg_ident) {
+            None => Err(CastleError::Validation(
+                format!(
+                    "{} was not specififed in input definition",
+                    join_paths(&[path, &[arg_ident]].concat()),
+                )
+                .into(),
+            ))?,
+            Some(input_def) => type_check_input_against_input_definition(
+                schema,
+                &[path, &[&**arg_ident]].concat(),
+                input_def,
+                input_value,
+            )?,
+        }
+    }
+
+    return Ok(());
+}
+
+
+/// check that all args defined in the [InputDefinitions] were used in the [Inputs]
+/// and that none were missing
+pub(crate) fn check_for_missing_args(
+    path: &[&str],
+    input_defs: &InputDefinitions,
+    inputs_map: &Inputs,
+) -> Result<(), CastleError> {
+    for (arg_ident, _) in input_defs.iter() {
+        match inputs_map.get(&**arg_ident) {
+            None => Err(CastleError::Validation(
+                format!(
+                    "{} was not specified in input, but is required",
+                    join_paths(&[path, &[arg_ident]].concat()),
+                )
+                .into(),
+            ))?,
+            Some(..) => {}
+        }
+    }
+
+    return Ok(());
 }
